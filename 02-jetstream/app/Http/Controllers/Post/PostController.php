7608ba55277f9494198e7beb8b9b8e7e52d10a38
Mitsuhito Fujita <mitsuhitofujita@gmail.com>
2023-02-12 07:47:59 +0000

    🚨 #11 Resolve PHPStan suggestions

--- 02-jetstream/app/Http/Controllers/Post/PostController.php
+++ 02-jetstream/app/Http/Controllers/Post/PostController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Post;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Http\Requests\Post\PostCreateRequest;
 use App\Http\Requests\Post\PostEditRequest;
@@ -11,36 +10,21 @@ use App\Http\Requests\Post\PostDestroyRequest;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Response;
+use Inertia\Response as InertiaResponse;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Post $post)
+    public function index(Post $post): InertiaResponse
     {
         return Inertia::render('Post/Index', ['posts' => $post->all()]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function create(): InertiaResponse
     {
         return Inertia::render('Post/Create', ['post' => ['payload' => '']]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\CreateRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(PostCreateRequest $request, Post $post)
+    public function store(PostCreateRequest $request, Post $post): Response
     {
         $post->fill([
             'user_id' => $request->getUserId(),
@@ -49,49 +33,20 @@ class PostController extends Controller
         return response('', Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(int $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(PostEditRequest $request, Post $post)
+    public function edit(PostEditRequest $request, Post $post): InertiaResponse
     {
         return Inertia::render('Post/Edit', ['post' => $post->find($request->getId())]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(PostUpdateRequest $request, Post $post)
+    public function update(PostUpdateRequest $request, Post $post): Response
     {
         $post->find($request->getId())->fill(['payload' => $request->getPayload()])->save();
         return response('', Response::HTTP_NO_CONTENT);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(PostDestroyRequest $request, Post $post)
+    public function destroy(PostDestroyRequest $request, Post $post): Response
     {
         $post->destroy($request->getId());
+        return response('', Response::HTTP_NO_CONTENT);
     }
 }
